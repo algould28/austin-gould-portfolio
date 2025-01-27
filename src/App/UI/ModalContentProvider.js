28bb3a22946b31ca469b20d9b2e7742cf19f1143
algould28 <austin.gould28@gmail.com>
2025-01-26 19:48:36 -0700

    updated contact me section

--- src/App/UI/ModalContentProvider.js
+++ src/App/UI/ModalContentProvider.js
@@ -19,7 +19,7 @@ export default class ModalContentProvider {
       contact: {
         title: "Contact Me",
         description:
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+          "Shoot me an email at <b>austin.gould28@gmail.com</b> or connect with me on <a target='_blank' href='https://www.linkedin.com/in/austin-gould-89299b170/'>LinkedIn</a>!",
       },
       code: {
         title: "Code Examples",
